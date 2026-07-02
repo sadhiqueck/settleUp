b25ec51e0346abf0e1cd514664911678c3f3d9de
Sadhique Ck <Sadhiquekpz@gmail.com>
2026-07-02 21:00:00 +0530

    fix:fronend bug fixes

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import axios from 'axios'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
+import "./index.css";
+import App from "./App.tsx";
 
 // Ensure Axios always sends cookies with requests
 axios.defaults.withCredentials = true;
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
@@ -19,4 +19,4 @@ createRoot(document.getElementById('root')!).render(
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
